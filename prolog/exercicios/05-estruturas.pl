ac99fcba7b23efc305b4f47fb28ba2a1e062c0d0
Celso Crivelaro <celsocrivelaro@gmail.com>
2023-11-13 20:59:24 -0300

    Update 05-estruturas.pl

--- prolog/exercicios/05-estruturas.pl
+++ prolog/exercicios/05-estruturas.pl
@@ -2,6 +2,10 @@
 
 % para pegar o último elemento
 % último
-ultimo([X|[]],X).
 % cada item da lista
 % descer com o tail
+
+Remover o n-ésimo elemento da lista
+Exemplo:
+?- remover([a,b,c,d,e,f,g,h,i,k],3,X).
+X = [a,b,d,e,g,h,k]
