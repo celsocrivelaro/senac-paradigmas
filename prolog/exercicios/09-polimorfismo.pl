9cf831b1fd4a31cec51e0150c41ecef37bcf2949
Celso Crivelaro <celsocrivelaro@gmail.com>
2023-11-27 20:16:38 -0300

    Create 09-polimorfismo.pl

---
+++ prolog/exercicios/09-polimorfismo.pl
@@ -0,0 +1,4 @@
+% função de calcular pedágio pelo tipo de veículo e eixos
+% carro(2) - carro sempre tem 2 eixos e preço fixo
+% moto(1) - moto sempre tem preço fixo
+% carreta(N) - carreta pode ter muitos eixo e o preço é * N (eixos)
