8502c1506e0e05112c5d2b2a8b59b7b987f389c1
Celso Crivelaro <celsocrivelaro@gmail.com>
2024-11-15 21:02:22 -0300

    Update 10-sudoku.pl

--- prolog/10-sudoku.pl
+++ prolog/10-sudoku.pl
@@ -1,33 +1,52 @@
+:- use_module(library(clpfd)).
+:- use_module(library(clpr)).
+
 valido([]).
 valido([L|T]) :- 
-    fd_all_different(L), valido(T).
+    all_distinct(L), valido(T).
 
 sudoku(Sudoku,Solucao) :-
     Solucao = Sudoku,
+    A11 in 1..4,
+    A12 in 1..4,
+    A13 in 1..4,
+    A14 in 1..4,
+    A21 in 1..4,
+    A22 in 1..4,
+    A23 in 1..4,
+    A24 in 1..4,
+    A31 in 1..4,
+    A32 in 1..4,
+    A33 in 1..4,
+    A34 in 1..4,
+    A41 in 1..4,
+    A42 in 1..4,
+    A43 in 1..4,
+    A44 in 1..4,
     Sudoku = [A11, A12, A13, A14,
               A21, A22, A23, A24,
               A31, A32, A33, A34,
               A41, A42, A43, A44],
-    fd_domain(Solucao, 1, 4),
     Linha1 = [A11, A12, A13, A14],
     Linha2 = [A21, A22, A23, A24],
     Linha3 = [A31, A32, A33, A34],
     Linha4 = [A41, A42, A43, A44],
-
     Coluna1 = [A11, A21, A31, A41],
     Coluna2 = [A12, A22, A32, A42],
     Coluna3 = [A13, A23, A33, A43],
     Coluna4 = [A14, A24, A34, A44],
-
     Quadrado1 = [A11, A12, A21, A22],
     Quadrado2 = [A13, A14, A23, A24],
     Quadrado3 = [A31, A32, A41, A42],
     Quadrado4 = [A33, A34, A43, A44],
-
+    
     valido([Linha1, Linha2, Linha3, Linha4,
     Coluna1, Coluna2, Coluna3, Coluna4,
-    Quadrado1, Quadrado2, Quadrado3, Quadrado4
-    ]).
+    Quadrado1, Quadrado2, Quadrado3, Quadrado4]).
 
 
-% sudoku([_,_,2,3,_,_,_,_,_,_,_,_,3,4,_,_],Solucao).
+%sudoku([1,_,3,_,
+%        _,_,_,2,
+%        3,_,_,_,
+%        _,1,_,_
+%         ],Resposta).
