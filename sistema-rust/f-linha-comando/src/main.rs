57190c591d1287e0cf31b895d01f2d89dcb7fc75
Celso Crivelaro <celso.crivelaro@quorum.us>
2025-08-25 17:51:23 -0300

    Linha de comando

--- sistema-rust/f-linha-comando/src/main.rs
+++ sistema-rust/f-linha-comando/src/main.rs
@@ -1,30 +1,11 @@
-use structopt::StructOpt;
-
-// 1 - Definindo uma estrutura da linha de comando
-#[derive(StructOpt)]
-struct Cli{
-  padrao: String,
-  #[structopt(parse(from_os_str))]
-  arquivo: std::path::PathBuf,
-}
+use std::env;
 
 fn main() {
-  // 2 - Padrões de entrada
-  //let padrao = std::env::args().nth(1).expect("no pattern given");
-  //let caminho = std::env::args().nth(2).expect("no path given");
-  // let args = Cli {
-  //   padrao: padrao,
-  //   arquivo: std::path::PathBuf::from(caminho),
-  // };
-  let args = Cli::from_args();
+    let args: Vec<String> = env::args().collect();
 
-  let content = std::fs::read_to_string(&args.arquivo)
-    .expect("could not read file");
+    let query = &args[1];
+    let file_path = &args[2];
 
-  for line in content.lines() {
-    if line.contains(&args.padrao) {
-      println!("{}", line);
-    }
-  }
-
+    println!("Searching for {query}");
+    println!("In file {file_path}");
 }
