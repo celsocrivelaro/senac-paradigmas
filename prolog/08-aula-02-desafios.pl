5bffc3b3ac616158540d169d487ca61e4577e78a
Celso Crivelaro <celsocrivelaro@gmail.com>
2025-10-27 16:03:12 -0300

    Update and rename 08-aula-02-desafios to 08-aula-02-desafios.pl

---
+++ prolog/08-aula-02-desafios.pl
@@ -0,0 +1,31 @@
+conectado(a, b).
+conectado(b, c).
+conectado(c, d).
+conectado(b, e).
+
+caminho(X, Y, [X, Y]) :- conectado(X, Y).
+caminho(X, Y, [X|T]) :-
+    conectado(X, Z),
+    caminho(Z, Y, T),
+    \+ member(X, T).
+
+% ?- caminho(a, e, Rota).
+% Rota = [a, b, e].
+
+
+ligacao(linha1, estacaoA, estacaoB).
+ligacao(linha1, estacaoB, estacaoC).
+ligacao(linha2, estacaoC, estacaoD).
+
+rota(Origem, Destino, [Origem, Destino]) :-
+    ligacao(_, Origem, Destino).
+rota(Origem, Destino, [Origem|Caminho]) :-
+    ligacao(_, Origem, Inter),
+    rota(Inter, Destino, Caminho),
+    \+ member(Origem, Caminho).
+
+% member(X, L) verifica se X pertence a lista L
+% \+ negador lógico. Assim, \+ member(X, L) verifica se X is not in L
+
+?- rota(estacaoA, estacaoD, Caminho).
+Caminho = [estacaoA, estacaoB, estacaoC, estacaoD]
