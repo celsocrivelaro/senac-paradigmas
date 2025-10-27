6de8a1ccdbd16f8dc34e2f0c8c8276dcd2ff0e1a
Celso Crivelaro <celsocrivelaro@gmail.com>
2025-10-27 15:19:18 -0300

    Update 06-listas-funcoes.pl
    delete file

--- prolog/06-listas-funcoes.pl
+++ prolog/06-listas-funcoes.pl
@@ -1,9 +1,1 @@
-concatenar([],L,L).
-concatenar([H|[]], Y, [H|Y]).
-concatenar([H|T], Y, [H|Z]) :- concatenar(T, Y, Z).
 
-pertence(X, [X|_]).
-pertence(X, [_ | Y]) :- pertence(X, Y).
-
-prefixo(X, Z) :- concatena(X, Y, Z).
-sufixo(Y, Z) :- concatena(X, Y, Z).
