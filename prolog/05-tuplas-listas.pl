7e149ef404e3be27a8210e53e05bb27ae81df156
Celso Crivelaro <celsocrivelaro@gmail.com>
2025-10-27 15:12:44 -0300

    Update 05-tuplas-listas.pl

--- prolog/05-tuplas-listas.pl
+++ prolog/05-tuplas-listas.pl
@@ -4,9 +4,9 @@
 (1, 2, 3) = (1, 2, 3, 4).  => false
 (A, B, C) = (1, 2, 3).  => Unificação
 (A, 1, 3) = (2, B, C).  => Unificação
-A = 2
-B = 1
-C = 3
+% A = 2
+% B = 1
+% C = 3
 (A, 1, 3) = (2, A, C).  => Unificação falha
 
 %% LISTAS
@@ -15,8 +15,8 @@ C = 3
 [_, 2, 3] = [1, 2, 3].  => true
 [1, 2, 3] = [1, 2, 3, 4].  => false
 (A, B, C) = [1, 2, 3].  => Unificação
-A = 1
-B = 2
+% A = 1
+% B = 2
 
 [1, 2, 3, 4] = [Head|Tail].
 % Head = 1,
