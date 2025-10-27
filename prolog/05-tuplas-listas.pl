d456904692d106bec391c49d9604fa0e3edf8f87
Celso Crivelaro <celsocrivelaro@gmail.com>
2025-10-27 15:12:19 -0300

    Update 05-tuplas-listas.pl

--- prolog/05-tuplas-listas.pl
+++ prolog/05-tuplas-listas.pl
@@ -1,28 +1,27 @@
 %% TUPLAS
 % executar
-% (1, 2, 3) = (1, 2, 3).  => true
-% (1, 2, 3) = (1, 2, 3, 4).  => false
-% (A, B, C) = (1, 2, 3).  => Unificação
-
-% (A, 1, 3) = (2, B, C).  => Unificação
+(1, 2, 3) = (1, 2, 3).  => true
+(1, 2, 3) = (1, 2, 3, 4).  => false
+(A, B, C) = (1, 2, 3).  => Unificação
+(A, 1, 3) = (2, B, C).  => Unificação
+A = 2
+B = 1
+C = 3
+(A, 1, 3) = (2, A, C).  => Unificação falha
 
 %% LISTAS
 % executar
-% [1, 2, 3] = [1, 2, 3].  => true
-% [1, 2, 3] = [1, 2, 3, 4].  => false
-% (A, B, C) = [1, 2, 3].  => Unificação
+[1, 2, 3] = [1, 2, 3].  => true
+[_, 2, 3] = [1, 2, 3].  => true
+[1, 2, 3] = [1, 2, 3, 4].  => false
+(A, B, C) = [1, 2, 3].  => Unificação
+A = 1
+B = 2
 
-% (A, 1, 3) = (2, B, C).  => Unificação
-% (A, 1, 3) = (2, A, C).  => Unificação falha
-
-% head / tail
-% [1, 2, 3, 4]
-% head = 1
-% tail = [2, 3 4]
-
-% [1, 2, 3, 4] = [Head|Tail].
+[1, 2, 3, 4] = [Head|Tail].
 % Head = 1,
 % Tail = [2, 3, 4].
 
-% [1, 2, 3, 4] = [_|[Head|_]].
+% Podemos testar estruturas complexas
+[1, 2, 3, 4] = [_|[Head|_]].
 % Head = 2.
